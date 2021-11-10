afdb0d22ed1e984e9e0112357f05b49e5c8fee5b
201702083 <201702083@o.cnu.ac.kr>
2021-11-11 00:14:52 +0900

    entry point

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	// "fmt"
+
+	// storage "github.com/Can-U-Join-Us/CUJU-Backend/modules/storage"
+
+	_ "github.com/Can-U-Join-Us/CUJU-Backend/modules/server" // Host serve when loaded
+)
+
+func main() {
+	//
+	// fmt.Println("[Init] CUJU-Server host")
+}
