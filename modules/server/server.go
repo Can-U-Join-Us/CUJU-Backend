d2a3b6d0998f36c57c8bcb11bcb56b8fa7b235c7
201702083 <201702083@o.cnu.ac.kr>
2021-11-17 00:03:14 +0900

    Mv redis setup to token.go

--- modules/server/server.go
+++ modules/server/server.go
@@ -1,17 +1,33 @@
 package server
 
 import (
+	"fmt"
+
 	_ "github.com/Can-U-Join-Us/CUJU-Backend/modules/storage"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 const port = ":4000"
 
+var ACCESS_SECRET string
+var REFRESH_SECRET string
+
 func init() { // local : 4000 호스팅 시작
 	r := gin.Default()
-
+	redisInit()
 	api := r.Group("/api")
 	registerApiHandlers(api)
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")    // optionally look for config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %w \n", err))
+	}
+	ACCESS_SECRET = viper.GetString(`token.ACCESS_SECRET`)
+	REFRESH_SECRET = viper.GetString(`token.REFRESH_SECRET`)
+
+	fmt.Println("\n\n", ACCESS_SECRET, REFRESH_SECRET, "\n\n")
 	r.Run(port)
 }
 func registerApiHandlers(api *gin.RouterGroup) {
@@ -29,15 +45,26 @@ func registerApiHandlers(api *gin.RouterGroup) {
 			c.JSON(200, gin.H{"error": nil})
 		}
 	})
-	/*  Reply			200 -> login pass
+	/*  Reply			200 -> token
 	400 -> ID or PW incorrect
 	*/
 	api.POST("/User/login", func(c *gin.Context) {
-		uid, err := loginUser(c)
+		token, err := loginUser(c)
 		if err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 		} else {
-			c.JSON(200, gin.H{"error": nil, "uid": uid})
+			c.JSON(200, gin.H{"error": nil, "token": token})
+		}
+	})
+	/*  Reply			200 -> token delete
+	400 -> ID or PW incorrect
+	*/
+	api.POST("/User/logout", func(c *gin.Context) {
+		err := logoutUser(c)
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(200, gin.H{"error": nil})
 		}
 	})
 	/*  Reply			200 -> register success
